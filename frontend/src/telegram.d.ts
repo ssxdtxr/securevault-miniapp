38cf99c14a7e50108042300e8d1dcb774e73491f
ssxdtxr <tereskinegor68@gmail.com>
2025-11-26 09:43:56 +0300

    feat: add base config f backend & frontend

---
+++ frontend/src/telegram.d.ts
@@ -0,0 +1,22 @@
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: {
+        ready(): void;
+        expand(): void;
+        close(): void;
+        initDataUnsafe?: {
+          user?: {
+            id: number;
+            first_name: string;
+            last_name?: string;
+            username?: string;
+            language_code?: string;
+          };
+        };
+      };
+    };
+  }
+}
+
+export {};
