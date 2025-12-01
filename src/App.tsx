0e3dd4e46a0f42cc8450ec2b23f6388d148930d7
ssxdtxr <tereskinegor68@gmail.com>
2025-12-01 18:48:57 +0300

    feat: add onClose

--- src/App.tsx
+++ src/App.tsx
@@ -7,49 +7,21 @@ import './App.css'
 //   name?: string | null
 // }
 
+const tg = window.Telegram?.WebApp
+
 function App() {
   const [userData, setUserData] = useState<any>(null)
 
   useEffect(() => {
-    // Проверяем, доступен ли объект Telegram Web App
-    if (window.Telegram && window.Telegram.WebApp) {
-      // Инициализируем Telegram Web App
-      window.Telegram.WebApp.ready()
-
-      // Получаем initData
-      const initData = window.Telegram.WebApp.initData
-
-      // Парсим данные пользователя
-      const user = parseInitData(initData)
-      setUserData(user)
-    } else {
-      console.error('Telegram Web App is not available')
-    }
+    tg?.ready()
   }, [])
 
-  const parseInitData = (initData: any) => {
-    const params = new URLSearchParams(initData)
-    return {
-      user: JSON.parse(params?.get('user') || '{ "user":"user" }'),
-      hash: params.get('hash'),
-      auth_date: params.get('auth_date'),
-      start_param: params.get('start_param'),
-      chat_type: params.get('chat_type'),
-      chat_instance: params.get('chat_instance'),
-    }
-  }
+  const onClose = () => tg?.close()
 
   return (
     <div>
       <h1>My Telegram Mini App</h1>
-      {userData ? (
-        <div>
-          <h2>User Info:</h2>
-          <pre>{JSON.stringify(userData, null, 2)}</pre>
-        </div>
-      ) : (
-        <p>Loading user data...</p>
-      )}
+      <button onClick={onClose}></button>
     </div>
   )
 }
