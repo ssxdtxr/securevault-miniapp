63acc87238407e55a7bb2e035c6ac3ce1586f25f
ssxdtxr <tereskinegor68@gmail.com>
2025-12-03 23:07:34 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from './shared/hooks/useAuth'
+// import { useAuth } from './shared/hooks/useAuth'
 import { Button } from './shared/ui/Button'
 import { Input } from './shared/ui/Input'
 
